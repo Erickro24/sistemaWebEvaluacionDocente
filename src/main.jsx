9e9ba2879e82351ad56a4ed8810e0fe8483f7c4a
erickrojas <naybra1994@gmail.com>
2026-05-08 17:48:15 -0400

    conexion front back y db

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import { BrowserRouter } from 'react-router'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Provider store={store}>
-          <BrowserRouter basename='/examenFinalReact-ERICK-ROJAS/'>
+          <BrowserRouter basename='/SistemaDeEvaluacion/'>
             <App />
           </BrowserRouter>
       </Provider>
